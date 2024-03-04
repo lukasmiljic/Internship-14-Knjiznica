259f9dd8bbed3ff1ed0c1ceafdfb69f5a4a7e0c6
lukasmiljic <smiljicluka13@gmail.com>
2024-03-04 18:16:28 +0100

    added search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import BookGrid from "./components/BookGrid/BookGrid.jsx";
 
 const App = () => {
+  const [search, setSearch] = useState("");
+
+  console.log(search)
+
   return (
     <>
       <div className="header-wrapper">
         <header>
           <h1>Books</h1>
+          <input
+            type="text"
+            placeholder="Search for books"
+            onChange={(e) => setSearch(e.target.value)}></input>
         </header>
       </div>
       <main>
-        <BookGrid />
+        <BookGrid search={search}/>
       </main>
     </>
   );
--- src/components/BookGrid/BookGrid.jsx
+++ src/components/BookGrid/BookGrid.jsx
@@ -1,12 +1,20 @@
 import Book from "../Book/Book";
 import books from "../../bookData";
 
-const BookGrid = () => {
+const BookGrid = (props) => {
+  const { search } = props;
+
   return (
     <div className="book-grid">
-      {books.map((book, index) => (
-        <Book key={index} {...book} />
-      ))}
+      {books
+        .filter((item) => {
+          return search.toLowerCase() === ""
+            ? item
+            : item.Title.toLowerCase().includes(search.toLowerCase());
+        })
+        .map((book, index) => (
+          <Book key={index} {...book} />
+        ))}
     </div>
   );
 };
