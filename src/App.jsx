1549aeee7bcdaee0bc6cb96211362ce34f9b37d5
lukasmiljic <smiljicluka13@gmail.com>
2024-03-04 18:29:48 +0100

    added styling to search bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ import BookGrid from "./components/BookGrid/BookGrid.jsx";
 const App = () => {
   const [search, setSearch] = useState("");
 
-  console.log(search)
+  console.log(search);
 
   return (
     <>
       <div className="header-wrapper">
         <header>
           <h1>Books</h1>
+          <i class="fa-solid fa-magnifying-glass"></i>
           <input
             type="text"
-            placeholder="Search for books"
+            placeholder="Search for books..."
             onChange={(e) => setSearch(e.target.value)}></input>
         </header>
       </div>
       <main>
-        <BookGrid search={search}/>
+        <BookGrid search={search} />
       </main>
     </>
   );
